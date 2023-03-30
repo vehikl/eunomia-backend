df99fdd276f5b6dd91f6cd9d956697f6ef92309e
Jacob Gardiner <j.gardiner@vehikl.com>
2023-03-30 09:08:55 -0400

    Added socket/data route

--- app/Http/Controllers/SocketController.php
+++ app/Http/Controllers/SocketController.php
@@ -2,16 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use BeyondCode\LaravelWebSockets\Apps\AppProvider;
+use BeyondCode\LaravelWebSockets\Dashboard\DashboardLogger;
+use Exception;
 use Illuminate\Broadcasting\Broadcasters\PusherBroadcaster;
 use Illuminate\Http\Request;
 use Pusher\Pusher;
 
 class SocketController extends Controller
 {
-    public function connect()
+    public function connect(Request $request)
     {
-        $broadcaster = new PusherBroadcaster(
-            new Pusher('', '', '', '')
-        );
+
+        try {
+            $broadcaster = new PusherBroadcaster(
+                new Pusher(env('PUSHER_APP_KEY'),
+                    env('PUSHER_APP_SECRET'),
+                    env('PUSHER_APP_ID'),
+                    []));
+        } catch (Exception $e) {
+            dd($e);
+        }
+
+        return $broadcaster->validAuthenticationResponse($request, []);
+    }
+
+    public function data(AppProvider $appProvider)
+    {
+        return [
+            "port" => env('PUSHER_PORT'),
+            "host" => env('PUSHER_HOST'),
+            "authEndpoint" => "api/sockets/connect",
+            "logChannel" => DashboardLogger::LOG_CHANNEL_PREFIX,
+            "apps" => $appProvider->all()
+        ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('socket/connect', [SocketController::class, 'connect']);
+
+Route::get('socket/data', [SocketController::class, 'data']);
